8d594f1a4761de0745a0c9fddba798adec75422a
Ismail Hanli <ismail.hanli@hotmail.com>
2019-05-05 13:48:32 +0200

    Scale player avatar

--- assets/js/player.js
+++ assets/js/player.js
@@ -4,9 +4,10 @@ define('player', ['pixi'], function (PIXI) {
 
     spriteSheet.baseTexture.width = 1024;
     spriteSheet.baseTexture.height = 1024;
-    spriteSheet.frame = new PIXI.Rectangle(143, 293, 104, 84);
+    spriteSheet.frame = new PIXI.Rectangle(143, 293, 103, 84);
 
     this.ship = new PIXI.Sprite(spriteSheet);
+    this.ship.scale.set(0.4);
   };
 
   Player.prototype.addToStage = function (stage) {
