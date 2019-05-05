27f20d2fd1c6bc76509953c1aef57da7ccbb2cec
Ismail Hanli <ismail.hanli@hotmail.com>
2019-05-05 13:37:31 +0200

    Add player avatar

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,31 +1,16 @@
 define('player', ['pixi'], function (PIXI) {
   function Player() {
-    // this.container = new PIXI.Container();
-    this.mask = new PIXI.Graphics();
+    let spriteSheet = PIXI.Texture.from('/assets/images/sheet.png');
 
-    // this.mask.x = 100;
-    // this.mask.y = 100;
-    // this.mask.width = 300;
-    // this.mask.height = 300;
+    spriteSheet.baseTexture.width = 1024;
+    spriteSheet.baseTexture.height = 1024;
+    spriteSheet.frame = new PIXI.Rectangle(143, 293, 104, 84);
 
-    this.sprite = PIXI.Sprite.fromImage('/assets/images/sheet.png');
-
-    this.mask.beginFill(0xFFFFFF);
-    this.mask.drawRect(0,0,this.sprite.width/this.sprite.scale.x,this.sprite.height/this.sprite.scale.y);
-    this.mask.endFill();
-
-    this.sprite.mask = this.mask;
-    this.sprite.addChild(this.mask);
-
-    // this.container.addChild(this.sprite);
-    // this.container.addChild(this.mask);
+    this.ship = new PIXI.Sprite(spriteSheet);
   };
 
   Player.prototype.addToStage = function (stage) {
-    // this.container.mask = this.mask;
-    // stage.addChild(this.container);
-    // stage.addChild(this.mask);
-    stage.addChild(this.sprite);
+    stage.addChild(this.ship);
   };
 
   return Player;
