02e61c464a6709166a13f6c5fc11d830fe956942
Ismail Hanli <ismail.hanli@hotmail.com>
2019-05-06 18:41:50 +0200

    Implement player avatar (#18)
    * WIP: player one
    
    * Add player avatar
    
    * Scale player avatar
    
    * Position player avatar
    
    * Rotate player avatar
    
    * Restructure code

--- assets/js/field.js
+++ assets/js/field.js
@@ -1,5 +1,6 @@
-define('field', ['pixi'], function (PIXI) {
+define('field', ['pixi', 'player'], function (PIXI, Player) {
     var BORDER_WIDTH = 5;
+    var HOME_BASE_HEIGHT = 70;
 
     return function Field(container) {
         this.app = new PIXI.Application({
@@ -8,14 +9,23 @@ define('field', ['pixi'], function (PIXI) {
             transparent: true
         });
 
-        let topLastLineOfDefense = this.app._options.height - 70;
+        let topLastLineOfDefense = this.app._options.height - HOME_BASE_HEIGHT;
 
         this.lastLineOfDefense = new PIXI.Graphics()
             .lineStyle(2, 0xffffff)
             .moveTo(0, topLastLineOfDefense)
             .lineTo(this.app._options.width, topLastLineOfDefense)
 
+        this.player = new Player();
+
+        this.player.setRotation(3.1415);
+        this.player.setPosition(
+          this.app._options.width / 2,
+          topLastLineOfDefense + HOME_BASE_HEIGHT / 2
+        );
+
         container.prepend(this.app.view);
         this.app.stage.addChild(this.lastLineOfDefense);
+        this.player.addToStage(this.app.stage);
     };
 });
