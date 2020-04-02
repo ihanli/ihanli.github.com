455f362d406bf8586f83b54ec512e4ba9fe56a62
Ismail Hanli <ismail.hanli@hotmail.com>
2020-04-02 18:43:46 +0200

    Complete health bar implementation

--- assets/js/components/player/healthBar.js
+++ assets/js/components/player/healthBar.js
@@ -1,17 +1,39 @@
 define('components/player/healthBar', ['pixi'], function (PIXI) {
+  const MAX_HEALTH = 269;
+
   function HealthBar() {
     let texture = PIXI.Texture.from('/assets/images/health.png');
-    this.sprite = new PIXI.Sprite(texture);
-    this.sprite.scale.set(0.2);
-    this.sprite.anchor.set(0.5, 0.5);
+    let sprite = new PIXI.Sprite(texture);
+    sprite.scale.set(0.25);
+    sprite.anchor.set(0.5, 0.5);
+
+    this.health = new PIXI.Graphics();
+    this.health.scale.set(0.2);
+
+    drawHealth(this.health, MAX_HEALTH);
+
+    this.container = new PIXI.Container();
+    this.container.addChild(sprite);
+    this.container.addChild(this.health);
   };
 
+  function drawHealth(health, value) {
+    health.clear();
+    health.beginFill(0x406271);
+    health.drawRoundedRect(-133, -19.5, value, 39, 3);
+    health.endFill();
+  }
+
   HealthBar.prototype.setPosition = function (x, y) {
-    this.sprite.position.set(x, y);
+    this.container.position.set(x, y);
   };
 
   HealthBar.prototype.getSprite = function () {
-    return this.sprite;
+    return this.container;
+  };
+
+  HealthBar.prototype.decreaseHealth = function (damage) {
+    drawHealth(this.health, MAX_HEALTH * damage / 100);
   };
 
   return HealthBar;
